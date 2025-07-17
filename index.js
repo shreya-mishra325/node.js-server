420079b1c447aadfaa797084ae0b3d19b1f7c8b6
Shreya Mishra <shreyamishra2k05@gmail.com>
2025-07-17 13:42:57 +0530

    server updated

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const API_KEY = '623878137ab28bb1f733637354e80ede';
 
     const server = http.createServer((req, res) => {
 
-    const parsedUrl = url.parse(req.url, true);
+        const parsedUrl = url.parse(req.url, true);
 
     if (parsedUrl.pathname === '/weather' && req.method === 'GET') {
         const city = parsedUrl.query.city;
 
         if (!city) {
             res.writeHead(400, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({error: 'City name not provided'}));
+            res.end(JSON.stringify({error: 'City name not provided. Please enter a valid city name.'}));
         }
 
         const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
@@ -29,7 +29,7 @@ const API_KEY = '623878137ab28bb1f733637354e80ede';
             const weather = JSON.parse(data);
 
             if (weather.cod !== 200) {
-                res.writeHead(weather.cod, { 'Content-Type': 'application/json' });
+                res.writeHead(weather.cod, {'Content-Type': 'application/json'});
                 res.end(JSON.stringify({ error: weather.message }));
                 return;
             }
@@ -42,23 +42,23 @@ const API_KEY = '623878137ab28bb1f733637354e80ede';
                 windspeed: weather.wind.speed,
             };
 
-            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.writeHead(200, {'Content-Type': 'application/json'});
             res.end(JSON.stringify(response));
             } catch (err) {
-            res.writeHead(500, { 'Content-Type': 'application/json' });
+            res.writeHead(500, {'Content-Type': 'application/json'});
             res.end(JSON.stringify({ error: 'Error parsing weather data' }));
             }
         });
         }).on('error', () => {
-        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.writeHead(500, {'Content-Type': 'application/json'});
         res.end(JSON.stringify({ error: 'Failed to fetch weather data' }));
         });
     } else {
         res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ error: 'Route not found' }));
+        res.end(JSON.stringify({error: 'Not found'}));
     }
     });
 
-    server.listen(5000, ()=>{
-    console.log(`Server running at http://localhost:5000/weather?city=cuttack`);
+    server.listen(7000, ()=>{
+    console.log(`Server running at http://localhost:7000/weather?city=cuttack`);
     });
